9a94cbd6ff04631e856d73b391d8920256563014
Joshua Zamora <54697058+ZanteZyaire@users.noreply.github.com>
2020-11-07 22:47:35 -0700

    Added my server url

--- .idea/bin/Main.dart
+++ .idea/bin/Main.dart
@@ -1,8 +1,7 @@
-import 'package:Connect4ConsoleApp/Controller.dart';
+import 'package:Connect4ConsoleApp/Control.dart';
 
 void main() {
-
+  // my url: https://cssrvlab01.utep.edu/classes/cs3360/jazamora6
   Controller controller = new Controller();
   controller.requestGame();
-
 }
