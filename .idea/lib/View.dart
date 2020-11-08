7ddf389cef313ab04283b705fd437343a9299da5
Joshua Zamora <54697058+ZanteZyaire@users.noreply.github.com>
2020-11-07 22:48:24 -0700

    Combined board into ConsoleUI class

--- .idea/lib/View.dart
+++ .idea/lib/View.dart
@@ -1,71 +1,63 @@
 import 'dart:io';
-import 'Model.dart';
 
 class ConsoleUI {
+  List<List<int>> _board;
 
-  Board _board;
+  ConsoleUI() {
+    this._board = List<List<int>>.generate(6, (i) =>
+    List<int>.generate(7, (i) => 0, growable: false), growable: false);
 
-  void setBoard(Board board) {
-    this._board = board;
   }
 
-  void showMessage(String message) {
-    print(message);
-  }
-
-  void showBoard(int lastMove) {
-    for(int i = 0; i < _board.height; i++) {
-      for(int j = 0; j < _board.width; j++) {
-        stdout.write(_board.getBoardSlots()[i][j].getSymbol() + ' ');
+  void insertDisc(int move, int player) {
+    for (int yCoordinate = 5; yCoordinate > -1; yCoordinate--) {
+      if (_board[yCoordinate][move] == 0) {
+        _board[yCoordinate][move] = player;
+        return;
       }
-      print('');
     }
-    for(int i = 1; i <= _board.width; i++) {
-      stdout.write(i.toString() + ' ');
-    }
-    print(' ');
-    if(lastMove >= 0) {
-      var marker = new List<String>.filled(_board.width, ' ', growable:false);
-      marker[lastMove] = '*';
-      print(marker.join(' '));
+  }
+
+  void printBoard() {
+    for(int i = 0; i < 6; i++) {
+      for(int j = 0; j < 7; j++) {
+        if (_board[i][j] == 0) stdout.write('. ');
+        else if (_board[i][j] == 1) stdout.write('H ');
+        else stdout.write('C ');
+      }
+        print('');
     }
   }
 
   String promptServer(String defaultURL) {
     while (true) {
       stdout.write('Enter the server URL [default: $defaultURL] ');
+
       var url = stdin.readLineSync();
-      if (url.isEmpty) {
-        return defaultURL;
-      }
-      else if (Uri
-          .parse(url)
-          .isAbsolute) {
-        return url;
-      }
-      showMessage('Invalid URL: $url');
+
+      if (url.isEmpty) return defaultURL;
+      else if (Uri.parse(url).isAbsolute) return url;
+      else print('Invalid URL: ' + url);
     }
   }
 
-  String promptStrategy(List strategies) {
+  String promptStrategy(var strategies) {
     while (true) {
-      stdout.write('Select the server strategy:');
-      for (var i = 0; i < strategies.length; i++) {
-        stdout.write(' ${i + 1}. ${strategies[i]}');
-      }
-      stdout.write(' [default: 1]');
+      stdout.write('Select the server strategy: 1. ' + strategies[0] + ' 2. ' + strategies[1] + ' [default: 1]: ');
+
       var line = stdin.readLineSync();
-      try {
-        if (line.isEmpty) {
+
+      switch (int.parse(line)) {
+        case 1:
+          print("Selected strategy: " + strategies[0]);
           return strategies[0];
-        }
-        var selection = int.parse(line);
-        if (selection >= 1 && selection <= strategies.length) {
-          return strategies[selection - 1];
-        }
-      } on FormatException {
+        case 2:
+          print("Selected strategy: " + strategies[1]);
+          return strategies[1];
+        default:
+          break;
       }
-      showMessage('Invalid selection: $line');
+      print('Invalid selection: ' + line);
     }
   }
 }
